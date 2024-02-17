c3c2dde47faa8c4e329ecd324f541325b6d8718c
tfkhdyt <tfkhdyt@proton.me>
2024-02-17 21:19:40 +0700

    feat(commit): add automated commit message generation for Gemini AI
    This commit adds an automated commit message generation feature to geminicommit.
    The feature uses the Google Gemini AI model to generate commit messages based on the diff between the current and previous commit.
    The commit messages follow the conventional commits specification and include a description, type, and scope.
    
    This feature is intended to help developers write more consistent and informative commit messages, which can improve the quality of the project's history and make it easier to understand the changes that have been made over time.

--- cmd/root.go
+++ cmd/root.go
@@ -4,31 +4,163 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"strings"
 
+	"github.com/charmbracelet/huh"
+	"github.com/charmbracelet/huh/spinner"
+	"github.com/google/generative-ai-go/genai"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"google.golang.org/api/option"
 
 	"github.com/tfkhdyt/geminicommit/cmd/config"
 )
 
-var cfgFile string
+var (
+	cfgFile  string
+	stageAll bool
+)
 
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "geminicommit",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A CLI that writes your git commit messages for you with Google Gemini AI ",
+	Long:  `A CLI that writes your git commit messages for you with Google Gemini AI `,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		if _, err := exec.Command("git", "-v").Output(); err != nil {
+			fmt.Println("Git is not installed")
+			os.Exit(1)
+		}
+
+		if _, err := exec.Command("git", "rev-parse", "--show-toplevel").Output(); err != nil {
+			fmt.Println("The current directory must be a Git repository")
+			os.Exit(1)
+		}
+
+		if stageAll {
+			if _, err := exec.Command("git", "add", "-u").Output(); err != nil {
+				fmt.Println("Failed to update tracked files")
+				os.Exit(1)
+			}
+		}
+
+		filesChan := make(chan []string, 1)
+		diffChan := make(chan string, 1)
+
+		if err := spinner.New().Title("Detecting staged files").Action(func() {
+			files, err := exec.Command("git", "diff", "--cached", "--diff-algorithm=minimal", "--name-only").Output()
+			if err != nil {
+				fmt.Println(err.Error())
+				os.Exit(1)
+			}
+			filesStr := strings.TrimSpace(string(files))
+
+			if filesStr == "" {
+				filesChan <- []string{}
+				diffChan <- ""
+				return
+			}
+
+			diff, err := exec.Command("git", "diff", "--cached", "--diff-algorithm=minimal").Output()
+			if err != nil {
+				fmt.Println(err.Error())
+				os.Exit(1)
+			}
+
+			filesChan <- strings.Split(filesStr, "\n")
+			diffChan <- string(diff)
+		}).Run(); err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+
+		files, diff := <-filesChan, <-diffChan
+
+		if len(files) == 0 {
+			fmt.Println("No staged changes found. Stage your changes manually, or automatically stage all changes with the `--all` flag")
+			os.Exit(1)
+		} else if len(files) == 1 {
+			fmt.Printf("Detected %d staged file:\n", len(files))
+		} else {
+			fmt.Printf("Detected %d staged files:\n", len(files))
+		}
+
+		for _, file := range files {
+			fmt.Println("    ", file)
+		}
+
+		messageChan := make(chan string, 1)
+		if err := spinner.New().Title("The AI is analyzing your changes").Action(func() {
+			ctx := context.Background()
+
+			client, err := genai.NewClient(ctx, option.WithAPIKey(viper.GetString("api.key")))
+			if err != nil {
+				fmt.Println(err.Error())
+				os.Exit(1)
+			}
+			defer client.Close()
+
+			model := client.GenerativeModel("gemini-pro")
+			resp, err := model.GenerateContent(ctx, genai.Text(fmt.Sprintf(`generate commit messages based on the following diff:
+%s
+
+commit messages should:
+ - follow conventional commits
+ - message format should be: <type>[scope]: <description>
+
+examples:
+ - fix(authentication): add password regex pattern
+ - feat(storage): add new test cases`, diff)))
+			if err != nil {
+				fmt.Println(err.Error())
+				os.Exit(1)
+			}
+
+			messageChan <- fmt.Sprintf("%v", resp.Candidates[0].Content.Parts[0])
+		}).Run(); err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+
+		message := <-messageChan
+		fmt.Println("\nChanges analyzed!")
+
+		if strings.TrimSpace(message) == "" {
+			fmt.Println("No commit messages were generated. Try again")
+			os.Exit(1)
+		}
+
+		fmt.Println(message)
+
+		var confirm bool
+		if err := huh.NewConfirm().
+			Title("Use this commit message?").
+			Affirmative("Yes!").
+			Negative("No.").
+			Value(&confirm).Run(); err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+
+		if !confirm {
+			fmt.Println("Commit cancelled")
+			return
+		}
+
+		if err := exec.Command("git", "commit", "-m", message).Run(); err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+
+		fmt.Println("✔ Successfully committed!")
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -49,6 +181,7 @@ func init() {
 	// will be global for your application.
 
 	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.config/geminicommit/config.toml)")
+	RootCmd.Flags().BoolVarP(&stageAll, "all", "a", false, "stage all changes in tracked files (default is false)")
 }
 
 // initConfig reads in config file and ENV variables if set.
--- go.mod
+++ go.mod
@@ -8,12 +8,40 @@ require (
 )
 
 require (
+	cloud.google.com/go/ai v0.3.0 // indirect
+	cloud.google.com/go/compute v1.23.3 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	cloud.google.com/go/longrunning v0.5.4 // indirect
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/catppuccin/go v0.2.0 // indirect
+	github.com/charmbracelet/bubbles v0.17.2-0.20240108170749-ec883029c8e6 // indirect
+	github.com/charmbracelet/bubbletea v0.25.0 // indirect
+	github.com/charmbracelet/huh v0.3.0 // indirect
+	github.com/charmbracelet/huh/spinner v0.0.0-20240209193029-45947515c4cf // indirect
+	github.com/charmbracelet/lipgloss v0.9.1 // indirect
+	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/generative-ai-go v0.7.0 // indirect
+	github.com/google/s2a-go v0.1.7 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
+	github.com/googleapis/gax-go/v2 v2.12.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
+	github.com/rivo/uniseg v0.4.6 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
@@ -21,11 +49,25 @@ require (
 	github.com/spf13/cast v1.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
+	go.opencensus.io v0.24.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
+	golang.org/x/crypto v0.16.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/oauth2 v0.15.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/term v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
+	google.golang.org/api v0.153.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20231106174013-bbf56f31fb17 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20231106174013-bbf56f31fb17 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231120223509-83a465c0220f // indirect
+	google.golang.org/grpc v1.59.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
