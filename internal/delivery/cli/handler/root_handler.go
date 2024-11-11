38f3497e31c5b51926d99e4436543f8d80ca95a5
Taufik Hidayat <tfkhdyt@proton.me>
2024-11-11 14:15:02 +0700

    refactor(delivery): Update root handler to use singleton pattern

--- internal/delivery/cli/handler/root_handler.go
+++ internal/delivery/cli/handler/root_handler.go
@@ -1,8 +1,10 @@
 package handler
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"sync"
 
 	"github.com/fatih/color"
 	"github.com/spf13/cobra"
@@ -15,23 +17,35 @@ type RootHandler struct {
 	useCase *usecase.RootUsecase
 }
 
-func NewRootHandler(useCase *usecase.RootUsecase) *RootHandler {
-	return &RootHandler{useCase}
+var (
+	rootHandlerInstance *RootHandler
+	rootHandlerOnce     sync.Once
+)
+
+func NewRootHandler() *RootHandler {
+	rootHandlerOnce.Do(func() {
+		useCase := usecase.NewRootUsecase()
+
+		rootHandlerInstance = &RootHandler{useCase}
+	})
+
+	return rootHandlerInstance
 }
 
 func (r *RootHandler) RootCommand(
+	ctx context.Context,
 	stageAll *bool,
 	userContext *string,
 	model *string,
 ) func(*cobra.Command, []string) {
 	return func(_ *cobra.Command, _ []string) {
 		modelFromConfig := viper.GetString("api.model")
-
 		if modelFromConfig != "" && *model == "" {
 			*model = modelFromConfig
 		}
 
-		if apiKey := viper.GetString("api.key"); apiKey == "" {
+		apiKey := viper.GetString("api.key")
+		if apiKey == "" {
 			fmt.Println(
 				"Error: API key is still empty, run this command to set your API key",
 			)
@@ -41,7 +55,7 @@ func (r *RootHandler) RootCommand(
 			os.Exit(1)
 		}
 
-		err := r.useCase.RootCommand(stageAll, userContext, model)
+		err := r.useCase.RootCommand(ctx, apiKey, stageAll, userContext, model)
 		cobra.CheckErr(err)
 	}
 }
