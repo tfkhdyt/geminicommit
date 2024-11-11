66454e0fd50a8c1a6254d0e0f133aa910f56f984
Taufik Hidayat <tfkhdyt@proton.me>
2024-11-11 07:06:13 +0700

    feat(service): Improve system and user prompt for commit message generation.

--- internal/service/gemini_service.go
+++ internal/service/gemini_service.go
@@ -2,7 +2,9 @@ package service
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/google/generative-ai-go/genai"
@@ -10,10 +12,74 @@ import (
 	"google.golang.org/api/option"
 )
 
-type GeminiService struct{}
+type GeminiService struct {
+	conventionalTypes map[string]string
+	systemPrompt      string
+}
 
 func NewGeminiService() *GeminiService {
-	return &GeminiService{}
+	return &GeminiService{
+		conventionalTypes: map[string]string{
+			"docs":     "Documentation only changes",
+			"style":    "Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)",
+			"refactor": "A code change that neither fixes a bug nor adds a feature",
+			"perf":     "A code change that improves performance",
+			"test":     "Adding missing tests or correcting existing tests",
+			"build":    "Changes that affect the build system or external dependencies",
+			"ci":       "Changes to our CI configuration files and scripts",
+			"chore":    "Other changes that don't modify src or test files",
+			"revert":   "Reverts a previous commit",
+			"feat":     "A new feature",
+			"fix":      "A bug fix",
+		},
+		systemPrompt: `You are a commit message generator that follows these rules:
+1. Write in present tense
+2. Be concise and direct
+3. Output only the commit message without any explanations
+4. Follow the format: <type>(<optional scope>): <commit message>`,
+	}
+}
+
+func (g *GeminiService) GetUserPrompt(
+	context *string,
+	diff string,
+	files []string,
+) (string, error) {
+	if context != nil {
+		temp := fmt.Sprintf("Use the following context to understand intent:\n%s", *context)
+		context = &temp
+	}
+
+	conventionalTypes, err := json.Marshal(g.conventionalTypes)
+	if err != nil {
+		return "", fmt.Errorf("error marshalling conventional types: %v", err)
+	}
+
+	return fmt.Sprintf(
+		`Generate a concise git commit message written in present tense for the following code diff with the given specifications below:
+The output response must be in format:
+<type>(<optional scope>): <commit message>
+
+Focus on being accurate and concise.
+
+%s
+
+Neighboring files:
+%s
+
+Commit message must be a maximum of 72 characters.
+
+Choose a type from the type-to-description JSON below that best describes the git diff: %s
+
+Exclude anything unnecessary such as translation. Your entire response will be passed directly into git commit.
+
+Code diff:
+%s`,
+		*context,
+		strings.Join(files, ", "),
+		conventionalTypes,
+		diff,
+	), nil
 }
 
 func (g *GeminiService) AnalyzeChanges(
@@ -22,73 +88,58 @@ func (g *GeminiService) AnalyzeChanges(
 	userContext *string,
 	relatedFiles *map[string]string,
 ) (string, error) {
-	client, err := genai.NewClient(
-		ctx,
-		option.WithAPIKey(viper.GetString("api.key")),
-	)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return "", err
-	}
-	defer client.Close()
 	// format relatedFiles to be dir : files
 	relatedFilesArray := make([]string, 0, len(*relatedFiles))
 	for dir, ls := range *relatedFiles {
 		relatedFilesArray = append(relatedFilesArray, fmt.Sprintf("%s:%s", dir, ls))
 	}
-	model := client.GenerativeModel("gemini-pro")
+
+	client, errClient := genai.NewClient(
+		context.Background(),
+		option.WithAPIKey(viper.GetString("api.key")),
+	)
+	if errClient != nil {
+		fmt.Printf("Error getting gemini client: %v", errClient)
+		os.Exit(1)
+	}
+
+	defer client.Close()
+
+	model := client.GenerativeModel("gemini-1.5-flash-latest")
+	// model.SafetySettings = []*genai.SafetySetting{
+	// 	{
+	// 		Category:  genai.HarmCategoryHateSpeech,
+	// 		Threshold: genai.HarmBlockNone,
+	// 	},
+	// 	{
+	// 		Category:  genai.HarmCategorySexuallyExplicit,
+	// 		Threshold: genai.HarmBlockNone,
+	// 	},
+	// 	{
+	// 		Category:  genai.HarmCategoryDangerousContent,
+	// 		Threshold: genai.HarmBlockNone,
+	// 	},
+	// 	{
+	// 		Category:  genai.HarmCategoryHarassment,
+	// 		Threshold: genai.HarmBlockNone,
+	// 	},
+	// 	{
+	// 		Category:  genai.HarmCategoryUnspecified,
+	// 		Threshold: genai.HarmBlockNone,
+	// 	},
+	// }
+	model.SystemInstruction = &genai.Content{
+		Parts: []genai.Part{genai.Text(g.systemPrompt)},
+	}
+
+	userPrompt, err := g.GetUserPrompt(userContext, diff, relatedFilesArray)
+	if err != nil {
+		return "", err
+	}
+
 	resp, err := model.GenerateContent(
 		ctx,
-		genai.Text(
-			fmt.Sprintf(
-				`let's assume you're an automated AI that will generate a conventional git commit message based on this diff changes:
-%s
-
-and this user context "%s".
-The commit message must follow this format:
-"<type>(<scope>): <description>
-
-[optional body]
-
-[optional footer(s)]"
-
-Neighboring Files:
-%s
-Type must be one of the following:
-- build: changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)
-- ci: changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)
-- docs: documentation only changes
-- feat: a new feature
-- fix: a bug fix
-- perf: a code change that improves performance
-- refactor: a code change that neither fixes a bug nor adds a feature
-- style: changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc. Changes to web styling do not fall into this category )
-- test: adding missing tests or correcting existing tests
-
-Specification:
-- Commits MUST be prefixed with a type, which consists of a noun, feat, fix, etc., followed by the OPTIONAL scope, OPTIONAL !, and REQUIRED terminal colon and space.
-- A scope MAY be provided after a type. A scope MUST consist of a noun describing a section of the codebase surrounded by parenthesis, e.g., fix(parser):
-- A description MUST immediately follow the colon and space after the type/scope prefix. The description is a short summary of the code changes, e.g., fix: array parsing issue when multiple spaces were contained in string.
-- A description MUST started with lowercase.
-- Descriptions CANNOT use past tense verbs.
-- A longer commit body MAY be provided after the short description, providing additional contextual information about the code changes. The body MUST begin one blank line after the description.
-- A commit body is free-form and MAY consist of any number of newline separated paragraphs.
-- One or more footers MAY be provided one blank line after the body. Each footer MUST consist of a word token, followed by either a :<space> or <space># separator, followed by a string value (this is inspired by the git trailer convention).
-- A footer’s token MUST use - in place of whitespace characters, e.g., Acked-by (this helps differentiate the footer section from a multi-paragraph body). An exception is made for BREAKING CHANGE, which MAY also be used as a token.
-- A footer’s value MAY contain spaces and newlines, and parsing MUST terminate when the next valid footer token/separator pair is observed.
-- Breaking changes MUST be indicated in the type/scope prefix of a commit, or as an entry in the footer.
-- If included as a footer, a breaking change MUST consist of the uppercase text BREAKING CHANGE, followed by a colon, space, and description, e.g., BREAKING CHANGE: environment variables now take precedence over config files.
-- If included in the type/scope prefix, breaking changes MUST be indicated by a ! immediately before the :. If ! is used, BREAKING CHANGE: MAY be omitted from the footer section, and the commit description SHALL be used to describe the breaking change.
-- Types other than feat and fix MAY be used in your commit messages, e.g., docs: update ref docs.
-- The units of information that make up Conventional Commits MUST NOT be treated as case sensitive by implementors, with the exception of BREAKING CHANGE which MUST be uppercase. 
-- BREAKING-CHANGE MUST be synonymous with BREAKING CHANGE, when used as a token in a footer.
-
-Exclude anything unnecessary, because your entire response will be passed directly into git commit`,
-				diff,
-				*userContext,
-				strings.Join(relatedFilesArray, ", "),
-			),
-		),
+		genai.Text(userPrompt),
 	)
 	if err != nil {
 		fmt.Println("Error:", err)
