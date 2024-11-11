6aeee21f9a3e35050a21b38924ac9f7969db5479
Taufik Hidayat <tfkhdyt@proton.me>
2024-11-11 10:20:55 +0700

    chore(release): update version to 0.2.0

--- cmd/root.go
+++ cmd/root.go
@@ -28,7 +28,7 @@ var RootCmd = &cobra.Command{
 	Use:     "geminicommit",
 	Short:   "CLI that writes your git commit messages for you with Google Gemini AI",
 	Long:    "CLI that writes your git commit messages for you with Google Gemini AI",
-	Version: "0.1.2",
+	Version: "0.2.0",
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Run: rootHandler.RootCommand(&stageAll, &userContext, &model),
