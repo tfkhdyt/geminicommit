1a0483aab5f115289cb427b61e81ceb7059d3bb0
Taufik Hidayat <tfkhdyt@proton.me>
2024-11-14 08:56:38 +0700

    fix(cli): uses model from config if --model is not used

--- internal/delivery/cli/handler/root_handler.go
+++ internal/delivery/cli/handler/root_handler.go
@@ -42,7 +42,7 @@ func (r *RootHandler) RootCommand(
 ) func(*cobra.Command, []string) {
 	return func(_ *cobra.Command, _ []string) {
 		modelFromConfig := viper.GetString("api.model")
-		if modelFromConfig != "" && *model == "" {
+		if modelFromConfig != "" && *model == "gemini-1.5-pro" {
 			*model = modelFromConfig
 		}
 
