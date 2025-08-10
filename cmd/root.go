a698d64262adbf2201a996bf06d83d6318a0272d
Taufik Hidayat <taufik@generativestudio.dev>
2025-08-10 20:20:18 +0700

    chore(version): bump to 0.5.0

--- cmd/root.go
+++ cmd/root.go
@@ -40,7 +40,7 @@ var RootCmd = &cobra.Command{
 	Use:     "gmc",
 	Short:   "CLI that writes your git commit messages for you with Google Gemini AI",
 	Long:    "CLI that writes your git commit messages for you with Google Gemini AI",
-	Version: "0.4.1",
+	Version: "0.5.0",
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Run: rootHandler.RootCommand(
