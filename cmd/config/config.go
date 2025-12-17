52077d5ef00fab4f9ff6edcb7f45b0ca0046a4c4
line1029 <line1029@naver.com>
2025-12-17 18:17:41 +0900

    feat(config): overhaul config command to support all flags
    Replaces specific subcommands with generic `get`, `set`, and `list`
    commands. Binds all CLI flags to the configuration, allowing users to
    persist settings like language, push, and dry-run.
    
    BREAKING CHANGE: `config` subcommands (`key`, `model`, `baseurl`) are removed. Use `gmc config set <key> <value>` instead.

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -5,10 +5,6 @@ package config
 
 import (
 	"github.com/spf13/cobra"
-
-	"github.com/tfkhdyt/geminicommit/cmd/config/baseurl"
-	"github.com/tfkhdyt/geminicommit/cmd/config/key"
-	"github.com/tfkhdyt/geminicommit/cmd/config/model"
 )
 
 // ConfigCmd represents the config command
@@ -16,20 +12,4 @@ var ConfigCmd = &cobra.Command{
 	Use:   "config",
 	Short: "Manage geminicommit configuration through cli",
 	Long:  `Manage geminicommit configuration through cli`,
-	Run: func(cmd *cobra.Command, args []string) {
-		// fmt.Println("config called")
-	},
-}
-
-func init() {
-	ConfigCmd.AddCommand(key.KeyCmd, model.KeyCmd, baseurl.BaseurlCmd)
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// configCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// configCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
